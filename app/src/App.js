faf1d819d7db5304fe6b972a28c3810094bd416b
WillBridge0789 <will.burbridge@student.awesomeinc.org>
2023-03-08 20:13:52 +0000

    Base components added. Switching to dev branch

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,15 @@
-import Footer from "./Footer";
-import Header from "./Header";
+import axios from 'axios';
 import NavBar from "./NavBar";
+import Header from "./Header";
+import Content from './Content';
+import Footer from "./Footer";
 
 function App() {
     return(
         <div>
             <NavBar />
             <Header />
+            <Content />
             <Footer />
             
         </div>
