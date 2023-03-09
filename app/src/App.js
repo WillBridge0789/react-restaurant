64561502093d88417e26960d0a41fc438c2ac0e8
WillBridge0789 <will.burbridge@student.awesomeinc.org>
2023-03-09 15:21:02 +0000

    Editing function for rendering menu items

--- app/src/App.js
+++ app/src/App.js
@@ -1,27 +1,29 @@
 import React, { useState, useEffect } from 'react';
-//import axios from 'axios';
+import axios from 'axios';
 import NavBar from "./NavBar";
 import Header from "./Header";
 import Menu from './Menu';
 import Footer from "./Footer";
 
 function App() {
-    const [data, setData] = useState({ hits: [] });
-    useEffect(async () => {
-        const result = await axios(
-            //api url
-        );
-
-
-    setData(result.data);
-  }, []);
-    return(
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        async function fetchData() {
+            // You can await here
+            const response = await axios.get('https://www.jsonkeeper.com/b/MDXW');
+            console.log(response.data);
+            setData(response.data);
+            // ...
+        }
+        fetchData();
+    }, []);
+    return (
         <div>
             <NavBar />
             <Header />
             <Menu />
             <Footer />
-            
+
         </div>
     )
 }
