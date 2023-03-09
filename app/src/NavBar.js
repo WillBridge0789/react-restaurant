c16822bf39f998ced6c39728ab0724a3a0403cab
WillBridge0789 <will.burbridge@student.awesomeinc.org>
2023-03-09 04:21:07 +0000

    small app.js element edit

--- app/src/NavBar.js
+++ app/src/NavBar.js
@@ -2,11 +2,11 @@ function NavBar() {
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">Navbar</a>
+                <a className="navbar-brand" href="#">Ethereal</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div className="collapse navbar-collapse d-flex justify-content-end" id="navbarNavAltMarkup">
                     <div className="navbar-nav">
                         <a className="nav-link active" aria-current="page" href="#">Menu</a>
                         <a className="nav-link" href="#">About</a>
