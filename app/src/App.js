34851f288098c37d01f4c2f2ca08e3ed6f48cc75
WillBridge0789 <will.burbridge@student.awesomeinc.org>
2023-03-09 04:12:52 +0000

    State and useEffect added, but currently not functional

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,20 @@
-import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+//import axios from 'axios';
 import NavBar from "./NavBar";
 import Header from "./Header";
 import Menu from './Menu';
 import Footer from "./Footer";
 
 function App() {
+    const [data, setData] = useState({ hits: [] });
+    useEffect(async () => {
+        const result = await axios(
+            //api url
+        );
+
+
+    setData(result.data);
+  }, []);
     return(
         <div>
             <NavBar />
